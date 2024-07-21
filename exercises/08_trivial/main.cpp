3116a1b5f44ca65ce8841938260b77137006f0c4
Bokun Zhang(张铂坤) <1537521192@qq.com>
2024-07-21 15:10:31 +0800

    练习提交

--- exercises/08_trivial/main.cpp
+++ exercises/08_trivial/main.cpp
@@ -8,17 +8,19 @@ struct FibonacciCache {
 
 // TODO: 实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (;; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    for (;; ++cache.cached) {
+        if (cache.cached >= i) break;
+        cache.cache[cache.cached] = cache.cache[cache.cached - 1] + cache.cache[cache.cached - 2];
     }
     return cache.cache[i];
 }
-
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
     // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
     // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
-    FibonacciCache fib;
+    FibonacciCache fib = {0};
+    fib.cache[0] = 0;
+    fib.cache[1] = 1;
     ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
     return 0;
